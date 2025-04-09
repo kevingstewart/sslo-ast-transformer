2909323b4995788df6fa62ea3da08095585a97f9
Kevin Stewart <kevin.g.stewart@gmail.com>
2025-04-09 14:39:05 -0400

    Update sslo-ast-transformer-installer.sh

--- sslo-ast-transformer-installer.sh
+++ sslo-ast-transformer-installer.sh
@@ -12,6 +12,7 @@ then
 fi
 
 ## Create the _sslo_ast_transformer rule
+echo "..Creating the _sslo_ast_transformer_rule iRule"
 rule=$(curl -sk ${IRULEURL})
 data="{\"name\":\"_sslo_ast_transformer_rule\",\"apiAnonymous\":\"${rule}\"}"
 curl -sk \
@@ -21,16 +22,19 @@ curl -sk \
 https://localhost/mgmt/tm/ltm/rule -o /dev/null
 
 ## Create the _sslo_ast_transformer_pool_internal pool
+echo "..Creating the _sslo_ast_transformer_pool_internal pool"
 ## --> points to transformer VIP
 tmsh create ltm pool _sslo_ast_transformer_pool_internal \
 members replace-all-with { 198.18.245.245:514 }
 
 ## Create the _sslo_ast_transformer_pool_external pool (leave empty on install)
 ## --> points to AST otel-collector
+echo "..Creating the _sslo_ast_transformer_pool_external pool"
 tmsh create ltm pool _sslo_ast_transformer_pool_external
 
 ## Create the _sslo_ast_transformer_log_dest HSL log destination
 ## --> points to internal pool --> VIP --> external pool
+echo "..Creating the _sslo_ast_transformer_log_dest log destination"
 tmsh create sys log-config destination remote-high-speed-log _sslo_ast_transformer_log_dest \
 protocol udp \
 distribution adaptive \
@@ -38,11 +42,13 @@ pool-name _sslo_ast_transformer_pool_internal
 
 ## Create the _sslo_ast_transformer_log_pub HSL log publisher
 ## --> points to log destination
+echo "..Creating the _sslo_ast_transformer_log_pub log publisher"
 tmsh create sys log-config publisher _sslo_ast_transformer_log_pub \
 destinations replace-all-with { _sslo_ast_transformer_log_dest }
 
 ## Create the _sslo_ast_transformer_logger access log (use this in SSLO log settings)
 ## --> points to log publisher
+echo "..Creating the _sslo_ast_transformer_logger"
 tmsh create apm log-setting _sslo_ast_transformer_logger \
 access replace-all-with { \
 general-log { \
@@ -55,6 +61,7 @@ publisher _sslo_ast_transformer_log_pub } }
 
 ## Create the _sslo_ast_transformer_vip virtual server
 ## --> pools to AST otel-collector
+echo "..Creating the _sslo_ast_transformer_vip virtual server"
 tmsh create ltm virtual _sslo_ast_transformer_vip \
 destination 198.18.245.245:514 \
 ip-protocol udp \
